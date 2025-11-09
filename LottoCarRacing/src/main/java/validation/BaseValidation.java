94db108aa81a55a813187414f4ce11fbaa6ab12c
yunajoe <yunajoe@gmail.com>
2025-11-09 17:09:15 +0900

    feat: 인풋 베이스 유효성 검증 로직

---
+++ LottoCarRacing/src/main/java/validation/BaseValidation.java
@@ -0,0 +1,48 @@
+package validation;
+
+// private는 같은 클래스에서마 사용가
+// protected 메서드는 같은 패키지혹은 상속을 받은 곳에서 오버라이딩 오버로드 가능하다.
+public class BaseValidation {
+	
+	protected void checkNull(String str) {
+    if (str == null) {
+        throw new IllegalArgumentException("입력값이 null입니다.");
+    }
+  }
+
+	
+	 protected void checkEmpty(String str) {
+	  if (str.isEmpty()) {
+        throw new IllegalArgumentException("문자열이 비어있습니다.");
+    }
+
+	}
+	
+
+	 protected void checkContainBlank(String str) {
+	    if (str.matches(".*\\s+.*")) {
+	        throw new IllegalArgumentException("문자열에 공백이 포함되어 있습니다.");
+	    }
+	}
+	
+  
+  public void validate(String str){
+  	
+  	
+    checkNull(str);
+    checkEmpty(str);
+    checkContainBlank(str);   
+  	
+
+  }
+ 
+  
+  
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
